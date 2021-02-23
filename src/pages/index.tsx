a0d081b605d5d5879a2020885a5855e3c02c17ff
Cleiton Alves <cleiton_jc@yahoo.com.br>
2021-02-22 22:01:12 -0300

    Home with Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import { NextPage } from 'next'
 
+import Header from '@/components/Header'
+
 const Home: NextPage = () => {
-  return <h1>Home</h1>
+  return (
+    <>
+      <Header />
+    </>
+  )
 }
 
 export default Home
