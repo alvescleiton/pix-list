3278720daa99e3662baad9139fe7870ef6ea5a97
Cleiton Alves <cleiton_jc@yahoo.com.br>
2021-02-22 22:00:07 -0300

    Search component

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Input } from './styles'
+
+const Search: React.FC = () => {
+  return (
+    <Container>
+      <Input type="search" placeholder="buscar" />
+    </Container>
+  )
+}
+
+export default Search
---
+++ src/components/Search/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+`
+
+export const Input = styled.input`
+  flex: 1 1 auto;
+  text-align: center;
+  padding: 10px;
+  border: 0;
+  box-shadow: 1px 4px 11px -2px #ccc;
+  background: #fafafa;
+  outline: none;
+`
