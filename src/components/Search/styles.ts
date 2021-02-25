214fdf9c443f66d4361651dd966580fa4dfcc84e
Cleiton Alves <cleiton_jc@yahoo.com.br>
2021-02-25 00:41:23 -0300

    Search layout

--- src/components/Search/styles.ts
+++ src/components/Search/styles.ts
@@ -5,17 +5,19 @@ export const Container = styled.div`
   top: 10px;
   left: 20px;
   right: 20px;
+  z-index: 51;
   display: flex;
   align-items: center;
   padding: 0 20px;
   width: calc(100% - 40px);
-  box-shadow: 0px 0px 4px 0px #ccc;
-  background: rgba(255,255,255, 0.7);
+  box-shadow: 0px 0px 2px 0px ${props => props.theme.colors.primary};
+  background: rgba(255,255,255, 0.93);
   border-radius: 8px;
 
   .fa-search {
     flex: 0 0 auto;
     margin-right: 20px;
+    color: ${props => props.theme.colors.primary};
   }
 `
 
@@ -24,5 +26,7 @@ export const Input = styled.input`
   font-size: 18px;
   line-height: 34px;
   border: 0;
+  color: ${props => props.theme.colors.primary};
+  background: transparent;
   outline: none;
 `
