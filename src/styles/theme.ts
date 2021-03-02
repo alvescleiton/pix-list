8175930a3de99c941b84df91d2449b2749484775
Cleiton Alves <cleiton_jc@yahoo.com.br>
2021-03-01 23:03:39 -0300

    Add new theme colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,10 @@
 const theme = {
   colors: {
     primary: '#800080',
-    primary_dark: '#710071'
+    primary_dark: '#710071',
+    second: '#008000',
+    second_light: '#03b803',
+    second_dark: '#017201'
   },
 }
 
