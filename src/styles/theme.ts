3c335306c5fb733eaa6ad750d5d3a429d1e60cfe
Cleiton Alves <cleiton_jc@yahoo.com.br>
2021-03-02 23:23:33 -0300

    Change second color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,8 +2,8 @@ const theme = {
   colors: {
     primary: '#800080',
     primary_dark: '#710071',
-    second: '#008000',
-    second_light: '#03b803',
+    second: '#05a705',
+    second_light: '#04d007',
     second_dark: '#017201'
   },
 }
