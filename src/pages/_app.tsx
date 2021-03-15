d42a59de671b5005d4fc96092ec232360cacd0b7
Cleiton Alves <cleiton_jc@yahoo.com.br>
2021-03-14 21:39:21 -0300

    Default layout with header and footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,15 @@ import { AppProps } from 'next/dist/next-server/lib/router/router'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
+      <Header />
       <Component {...pageProps} />
+      <Footer />
       <GlobalStyle />
     </ThemeProvider>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,15 @@
 import { NextPage } from 'next'
 
-import Header from '@/components/Header'
+
 import PixList from '@/components/Pix/PixList'
 import Search from '@/components/Search'
-import Footer from '@/components/Footer'
 
 const Home: NextPage = () => {
   return (
     <>
-      <Header />
-
       <Search />
 
       <PixList />
-
-      <Footer />
     </>
   )
 }
