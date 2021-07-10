34e00b0c9dcf00a0a0f4450341e6a2b0a5ef618d
Cleiton Alves <cleiton@sorteonline.com.br>
2021-07-09 21:27:53 -0300

    Pix Form

---
+++ src/components/Pix/PixForm/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { FormControl, Input, InputLabel, Select, MenuItem, Button } from '@material-ui/core';
+import { Container, Title } from './styles'
+
+const PixForm = () => {
+  return (
+    <Container>
+      <Title>Cadastro</Title>
+
+      <form onSubmit={() => false}>
+        <FormControl fullWidth margin="normal">
+          <InputLabel htmlFor="nome">Nome</InputLabel>
+          <Input id="nome" aria-describedby="nome" />
+        </FormControl>
+
+        <FormControl fullWidth margin="normal">
+          <InputLabel htmlFor="tipo">Tipo</InputLabel>
+          <Select
+            id="tipo"
+          >
+            <MenuItem value={1}>E-mail</MenuItem>
+            <MenuItem value={2}>CPF</MenuItem>
+            <MenuItem value={3}>Telefone</MenuItem>
+            <MenuItem value={4}>Chave Gerada</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl fullWidth margin="normal">
+          <InputLabel htmlFor="chave_pix">Chave PIX</InputLabel>
+          <Input id="chave_pix" aria-describedby="chave_pix" />
+        </FormControl>
+
+        <FormControl fullWidth margin="normal">
+          <InputLabel htmlFor="observacao">Observação</InputLabel>
+          <Input id="observacao" aria-describedby="observacao" />
+        </FormControl>
+
+        <FormControl fullWidth margin="normal"></FormControl>
+
+        <FormControl fullWidth margin="normal">
+          <Button variant="contained" color="primary" size="large" disableElevation>
+            Gravar
+          </Button>
+        </FormControl>
+      </form>
+    </Container>
+  )
+}
+
+export default PixForm
---
+++ src/components/Pix/PixForm/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  padding: 15px;
+`
+
+export const Title = styled.h1`
+  display: block;
+  text-align: center;
+  font-size: 20px;
+  color: #583158;
+  font-weight: 700;
+  text-transform: uppercase;
+`
