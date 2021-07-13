d87a20e89d259daf8e5ec3beb81456de054435d9
Cleiton Alves <cleiton_jc@yahoo.com.br>
2021-07-13 18:25:17 -0300

    Change pix type structure

--- src/shared/types/pix.ts
+++ src/shared/types/pix.ts
@@ -1,12 +1,6 @@
 export type PixItemInterface = {
   name: string
   description: string | null
-  pix: PixInfoInterface[]
-}
-
-export type PixInfoInterface = {
   type: number
-  title: string
-  value: string
-  description?: string
+  pixKey: string
 }
