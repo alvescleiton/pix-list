eaff997dec477a35f3eae3c49a2aa01c7e3b00e4
Cleiton Alves <cleiton_jc@yahoo.com.br>
2021-07-16 17:54:59 -0300

    PixTypesList

---
+++ src/shared/consts/index.ts
@@ -0,0 +1,18 @@
+export const PixTypesList = {
+  EMAIL: {
+    id: 1,
+    title: 'E-mail',
+  },
+  CPF: {
+    id: 2,
+    title: 'CPF',
+  },
+  TELEFONE: {
+    id: 3,
+    title: 'Telefone',
+  },
+  CHAVE_GERADA: {
+    id: 4,
+    title: 'Chave Gerada',
+  },
+}
