2cdf461b4e7692a90c06371fbe61f8c6595f1c65
Cleiton Alves <cleiton_jc@yahoo.com.br>
2021-07-18 22:36:32 -0300

    No item to show

---
+++ src/components/NoItemsToShow/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from './styles'
+
+const NoItemsToShow = () => {
+  return (
+    <Container>
+      Nenhum item a ser exibido
+    </Container>
+  )
+}
+
+export default NoItemsToShow
