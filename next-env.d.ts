0e39255b7ee2044318ff84077336f13fad40064e
Cleiton Alves <cleiton_jc@yahoo.com.br>
2021-07-21 22:38:34 -0300

    Mongodb and next-env changes

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,13 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next-images" />
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      mongo: any;
+    }
+  }
+}
+
+export { }
